b96067701830f3d85d485d011e6cba08e75eeb1a
farizan17 <muhammadfarizankholis@gmail.com>
2022-06-07 22:38:04 +0700

    framework commit

---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header className="header-wrapper">
+      <img src="/gwblogo.png" alt="Logo" />
+      <div className="navigation">
+          <p>Masuk</p>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/SideBar/sidebar.js
@@ -0,0 +1,14 @@
+import "./sidebar.css";
+
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Transaksi</li>
+        <li>Edukasi Pengolahan Sampah</li>
+        <li>Penukaran Poin</li>
+        <li>Histori Transaksi</li>
+      </ul>
+    </div>
+  );
+}
