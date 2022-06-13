8a1665c4ce912486c873be8c3c029a832ab6c6ed
farizan17 <muhammadfarizankholis@gmail.com>
2022-06-13 16:13:12 +0700

    restore

--- src/pages/histori.js
+++ src/pages/histori.js
@@ -1,7 +1,35 @@
+import "./histori.css";
+
 export default function histori() {
+<<<<<<< Updated upstream
     return (
       <div className="page">
         <h1>histori</h1>
+=======
+  return (
+    <div className="sub-container">
+      <img
+        className="sidepanel-img"
+        src="/images/leftpanel.png"
+        alt="sidepanel"
+      />
+      <div className="content">
+        <div className="content-title">
+          <h1>Aktivitas Saya</h1>
+          <div className="indikator-wrapper">
+            <div className="trash-amount">
+              <h1>0</h1>
+            </div>
+          </div>
+        </div>
+        <div className="main-content"></div>
+>>>>>>> Stashed changes
       </div>
-    );
-  }
+      <img
+        className="sidepanel-img"
+        src="/images/rightpanel.png"
+        alt="sidepanel"
+      />
+    </div>
+  );
+}
