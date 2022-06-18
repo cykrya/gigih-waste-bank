89f5434ec4f3d6d6fde33d66d02b00fdc5cb1f95
Cykrya <joseanthony360@gmail.com>
2022-06-18 07:31:42 +0700

    added redux for links to profile

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -10,6 +10,10 @@ import WcIcon from '@mui/icons-material/Wc';
 import NumbersIcon from '@mui/icons-material/Numbers';
 import HomeIcon from '@mui/icons-material/Home';
 
+import { useSelector, useDispatch } from 'react-redux';
+import {emailNotelp, password,nama,umur,alamat,jeniskel} from '../components/core/slice'
+
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -23,41 +27,77 @@ const theme = createTheme({
 });
 
 const Gender = [
-    {
-      value: 'Male',
-      label: 'Male',
-    },
-    {
-      value: 'Female',
-      label: 'Female',
-    },
-    {
-      value: 'Others',
-      label: 'Others',
-    },
-  ];
+  {
+    value: 'Male',
+    label: 'Male',
+  },
+  {
+    value: 'Female',
+    label: 'Female',
+  },
+  {
+    value: 'Others',
+    label: 'Others',
+  },
+];
 
-  
+
+
+// export default function Register() {
+  const Register = () => {
+  const [profemailPhone, setprofEmailPhone] = useState("");
+  const [profpassword, setprofPassword] = useState("");
+  const [profname, setprofName] = useState("");
+  const [profage, setprofAge] = useState("");
+  const [profaddress, setprofAddress] = useState("");
+  const [profgender, setprofGender] = useState('Male');
+  const dispatch = useDispatch()
+
+  const InsertProfileData = (e) => {
+    e.preventDefault();
+    console.log("searching track");
+    console.log(profemailPhone);
+    console.log(profpassword);  
+    console.log(profname);  
+    console.log(profage);  
+    console.log(profaddress);  
+    console.log(profgender);
+    dispatch(emailNotelp(profemailPhone));
+    dispatch(password(profpassword));  
+    dispatch(nama(profname));  
+    dispatch(umur(profage));  
+    dispatch(alamat(profaddress));  
+    dispatch(jeniskel(profgender));    
     
-export default function Register() {
-   
-  const [genderselect, setGender] = useState('Male');
-
-  const handleChange = (event) => {
-    setGender(event.target.value);
   };
+
+  const test1=((useSelector((state)=>state.profile)).emailNotelp);
+  const test2=((useSelector((state)=>state.profile)).password);
+  const test3=((useSelector((state)=>state.profile)).nama);
+  const test4=((useSelector((state)=>state.profile)).umur);
+  const test5=((useSelector((state)=>state.profile)).alamat);
+  const test6=((useSelector((state)=>state.profile)).jeniskel);
+  
+  useEffect (()=>{
+    console.log("nama akun: "+test1);
+    console.log("nama password: "+test2);
+    console.log("nama user: "+test3);
+    console.log("umur: "+test4);
+    console.log("nama alamat: "+test5);
+    console.log("jenis kelamin: "+test6);
+  });
+
 
     return (
       <ThemeProvider theme={theme}>
         <div className="page register">
           <div className="registerWrapper"> 
-            <form className="playlistForm">
+            <form onSubmit={InsertProfileData} className="playlistForm">
               <FormControl className="ssearch">
-
                 <Box mt={0.5} sx={{m:1, pt:3} }>
-                  <IconTextField
-                    label="Email / No Telp"
-                    type="text"
+                  <IconTextField onChange={(e) => setprofEmailPhone(e.target.value)}
+                    label="Email/No Telp"
+                    type="email"
                     size="small"
                     sx={{width: '50ch',fontSize: 12}}
                     iconStart={<EmailIcon sx={{ color: "black"}}/>}
@@ -66,7 +106,7 @@ export default function Register() {
 
                 <Box mt={0.5} sx={{mt:1,mb:0.3}}>
                   <span>
-                    <IconTextField
+                    <IconTextField  onChange={(e) => setprofPassword(e.target.value)}
                       label="Password"
                       type="password"
                       size="small"
@@ -77,7 +117,7 @@ export default function Register() {
                 </Box>
                 
                 <Box mt={0.5} sx={{m:1, mb:0.3} }>
-                  <IconTextField
+                  <IconTextField  onChange={(e) => setprofName(e.target.value)}
                     label="Nama"
                     type="text"
                     size="small"
@@ -87,7 +127,7 @@ export default function Register() {
                 </Box>
                 
                 <Box mt={0.5} sx={{m:1, mb:0.3} }>
-                  <IconTextField
+                  <IconTextField  onChange={(e) => setprofAge(e.target.value)}
                     label="umur"
                     type="number"
                     size="small"
@@ -97,7 +137,7 @@ export default function Register() {
                 </Box>
 
                 <Box mt={0.5} sx={{m:1,mb:0.3} }>
-                  <IconTextField
+                  <IconTextField  onChange={(e) => setprofAddress(e.target.value)}
                     label="alamat"
                     type="text"
                     size="small"
@@ -107,21 +147,13 @@ export default function Register() {
                 </Box>
 
                 <Box mt={0.5} sx={{m:1, mb:0.3} }>
-                  {/* <IconTextField
-                    label="Jenis kelamin"
-                    type="text"
-                    size="small"
-                    sx={{width: '50ch',fontSize: 12}}
-                    iconStart={<EmailIcon sx={{ color: "black"}}/>}
-                  /> */}
-
                   <TextField
                     id="outlined-select-currency"
                     select
                     label="jenis kelamin"
                     size="small"
-                    value={genderselect}
-                    onChange={handleChange}
+                    value={profgender}
+                    onChange={(e) => setprofGender(e.target.value)}
                     sx={{ width:'50ch',fontSize: 12}}
                     InputProps={{
                       startAdornment: (
@@ -140,9 +172,9 @@ export default function Register() {
                 </Box>
 
                 <Box mt={0.5} sx={{mt:3,mb:0.3}}>
-                  <Link to="/home">
+                  {/* <Link to="/home"> */}
                     <Button variant="contained" type="input"  className="button register" sx={{ color: 'white' ,width:150, borderRadius: '25px'}}>Masuk</Button>
-                  </Link>
+                  {/* </Link> */}
                 </Box>
                 
               </FormControl>
@@ -169,3 +201,5 @@ export default function Register() {
       />
     );
   };
+
+  export default Register;
