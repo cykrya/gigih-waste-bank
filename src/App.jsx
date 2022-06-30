a2ed1c211235aa1523f0ab5a9d600471e96843c7
vaniaalvita <alvitavania17@gmail.com>
2022-06-30 19:46:59 +0700

    halaman penukaran poin

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Register from './pages/loginregis/register';
 import Transaksi from './pages/transaksi/transaksi';
 import Edukasi from './pages/edukasi/edukasi';
 import PenukaranPoin from './pages/penukaranpoin/penukaranpoin';
+import HistoriPoin from './pages/penukaranpoin/historipoin';
 import Histori from './pages/histori/histori';
 import Profile from './pages/profile/profile';
 
@@ -29,6 +30,7 @@ const App = () => {
             <Route exact path="/transaksi" element={<Transaksi />} />
             <Route exact path="/edukasi" element={<Edukasi />} />
             <Route exact path="/penukaranpoin" element={<PenukaranPoin />} />
+            <Route exact path="/historipoin" element={<HistoriPoin />} />
             <Route exact path="/histori" element={<Histori />} />
             <Route exact path="/profile" element={<Profile />} />
           </Routes>
