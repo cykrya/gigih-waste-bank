a13775ca183c28c24ccc020c8f978ae3e5c7e3af
Cykrya <joseanthony360@gmail.com>
2022-06-30 19:41:03 +0700

    added edukasi page (unfinished)

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,13 @@ import Home from './pages/home/home';
 import Login from './pages/loginregis/login';
 import Register from './pages/loginregis/register';
 import Transaksi from './pages/transaksi/transaksi';
-import Edukasi from './pages/edukasi/edukasi';
+import Edukasi from './pages/edukasi/Edukasi';
 import PenukaranPoin from './pages/penukaranpoin/penukaranpoin';
 import Histori from './pages/histori/histori';
 import Profile from './pages/profile/profile';
-
-
+import A1 from './pages/edukasi/edukasiPage/A1';
+import A2 from './pages/edukasi/edukasiPage/A2';
+import A3 from './pages/edukasi/edukasiPage/A3';
 
 const App = () => {
   return (
@@ -31,6 +32,9 @@ const App = () => {
             <Route exact path="/penukaranpoin" element={<PenukaranPoin />} />
             <Route exact path="/histori" element={<Histori />} />
             <Route exact path="/profile" element={<Profile />} />
+            <Route exact path="/Aone" element={<A1/>} />
+            <Route exact path="/Atwo" element={<A2/>} />
+            <Route exact path="/Athree" element={<A3/>} />
           </Routes>
         </div>
       </Router>
---
+++ src/pages/loginregis/alerts/RegisterSuccess.jsx
@@ -0,0 +1,29 @@
+import { Alert, Box, Collapse } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#857864',
+    },
+    secondary: {
+      main: '#11cb5f',
+    },
+  },
+});
+
+const RegisterSuccess = ({Open}) => {
+return(
+    <ThemeProvider theme={theme}>
+      <Box sx={{width:'500px',zIndex: 'tooltip', position: 'absolute',display:'inline-block', mt:2,ml:4 }}>
+        <Collapse in={Open}>
+          <Alert variant="filled" severity="success"  sx= {{mb:2}}>
+              Signed in!
+          </Alert>
+        </Collapse>
+      </Box>
+    </ThemeProvider>
+  )
+};
+
+export default RegisterSuccess;
