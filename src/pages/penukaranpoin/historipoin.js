f8986b4cf76a66e1645d59cc56b2c537c8dcb7a7
farizan17 <muhammadfarizankholis@gmail.com>
2022-07-02 23:36:35 +0700

    fix api url and some code

--- src/pages/penukaranpoin/historipoin.js
+++ src/pages/penukaranpoin/historipoin.js
@@ -16,7 +16,7 @@ export default function HistoriPoin() {
   useEffect(() => {
     const getData = async () => {
       try {
-        const res = await fetch("http://localhost:1337/api/points?populate=*&filters[user_id][id]=4").then(
+        const res = await fetch("https://gwb-api.herokuapp.com/api/points?populate=*&filters[user_id][id]=5").then(
           (response) => response.json()
         );
         setData(res.data);
