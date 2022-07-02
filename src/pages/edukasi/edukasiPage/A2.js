8ecff411f7a68289b87f6a101fb2f5d00e22ade7
Cykrya <joseanthony360@gmail.com>
2022-07-02 23:31:28 +0700

    added json for edukasi page's data

--- src/pages/edukasi/edukasiPage/A2.js
+++ src/pages/edukasi/edukasiPage/A2.js
@@ -1,13 +1,17 @@
 import "./article.css";
-
+import data from "./data.json";
 export default function A2() {
+  const title= data[1].title;
+  const paragraph= data[1].paragraphs;
+  const img= data[0].img;
     return (
       <div className="page-edukasi edukasi-container">
       <div className="A-Wrapper">
-        <h1 className="A-title">Jenis - Jenis Sampah Organik dan Anorganik</h1>
-        <img src='/img/c2.png' alt="jpg" className='icon c2'></img>  
+        <h1 className="A-title">{title}</h1>
+        <img src={img} alt="jpg" className='icon c2'></img>  
         <article className="A-content">
-          <h2>Jenis – Jenis Sampah Organik</h2>
+        <div dangerouslySetInnerHTML={{ __html: paragraph}}/>
+          {/* <h2>Jenis – Jenis Sampah Organik</h2>
             <h3>1. Sampah Organik Basah</h3>
             Sampah organik basah merupakan sampah organik yang mengandung banyak air.contoh dari jenis sampah organic basah yakni 
             seperti  sisa sisa sayur, kulit pisang, buah yang sudah busuk, kulit bawang dan sejenisnya.
@@ -29,7 +33,7 @@ export default function A2() {
             Sampah anorganik keras adalah jenis sampah anorganik yang memiliki kandungan bahan yang sulit untuk dihancurkan dan 
             sifatnya lebih kuat jika dibandingkan dengan jenis yang lunak. Limbah ini rata rata sulit untuk mengolahnya kembali. 
             Jika ingin melakukan pendaur ulangan pada sampah anorganik keras ini diperlukan Alat dan mesin yang lebih komplek. 
-            Contoh dari jenis sampah anorganik keras, antara lain sampah kaca,kaleng,bahan-bahan berdasar metal,dll.
+            Contoh dari jenis sampah anorganik keras, antara lain sampah kaca,kaleng,bahan-bahan berdasar metal,dll. */}
 
         </article>
       </div>
